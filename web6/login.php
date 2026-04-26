d1d468670a882efbb1b04f5405eb043289d3105b
s0196071 <kisyayo@yandex.ru>
2026-04-26 23:06:06 +0300

    Update login.php

--- web6/login.php
+++ web6/login.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Подключение к БД
-$db = new PDO("mysql:host=localhost;dbname=u68775", 'u68775', '7631071', [
+$db = new PDO("mysql:host=localhost;dbname=u82389", 'u82389', '3736104', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
