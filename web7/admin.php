73d7f950439e0cd6110bf932d8ec4c1c7d2ea0ba
s0196071 <kisyayo@yandex.ru>
2026-05-13 22:27:39 +0300

    Update admin.php

--- web7/admin.php
+++ web7/admin.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);   // только при HTTPS
-ini_set('session.cookie_samesite', 'Lax');
-session_set_cookie_params([
-    'lifetime' => 0,
-    'path' => '/',
-    'domain' => null,
-    'secure' => true,
-    'httponly' => true,
-    'samesite' => 'Lax'
-]);
+require_once 'session.php';
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="Admin Panel"');
