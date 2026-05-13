664c380ff12bf4c71a34f69e16e99b3e64096bae
s0196071 <kisyayo@yandex.ru>
2026-05-13 22:09:59 +0300

    Create basecss.php

---
+++ web7/basecss.php
@@ -0,0 +1,28 @@
+.container{
+    width: 70%;
+    padding: 20px;
+    
+}
+.highlighted {
+    background-color: #FFEBEE;
+} 
+
+header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+.success-mes{
+    color: white;
+    font: bold;
+    background-color: #FF78C9;
+    text-align: center;
+    margin-top: 15px;
+    padding: 10px;
+    border-radius: 5px;
+}
+.error{
+    color: red;
+    font-size: 0.9em;
+    margin-top: 5px;
+}
