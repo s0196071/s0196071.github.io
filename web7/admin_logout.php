61453705df66b6570d45af1f2066011b8c83f65e
s0196071 <kisyayo@yandex.ru>
2026-05-13 22:28:57 +0300

    Update admin_logout.php

--- web7/admin_logout.php
+++ web7/admin_logout.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);   // только при HTTPS
-ini_set('session.cookie_samesite', 'Lax');
-session_set_cookie_params([
-    'lifetime' => 0,
-    'path' => '/',
-    'domain' => null,
-    'secure' => true,
-    'httponly' => true,
-    'samesite' => 'Lax'
-]);
+require_once 'session.php';
 
 // Подключение к БД
 $db = new PDO("mysql:host=localhost;dbname=u82389", 'u82389', '3736104', [
