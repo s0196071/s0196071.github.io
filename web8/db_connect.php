b5fdfcd4e687b577f14b9113410600e137ce7443
s0196071 <kisyayo@yandex.ru>
2026-05-20 19:07:05 +0300

    Create db_connect.php

---
+++ web8/db_connect.php
@@ -0,0 +1,6 @@
+<?php
+$db = new PDO("mysql:host=localhost;dbname=u82389", 'u82389', '3736104', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
+?>
