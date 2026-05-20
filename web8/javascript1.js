94095d15681f07c60e6b8312de34932034b35e92
s0196071 <kisyayo@yandex.ru>
2026-05-20 12:58:27 +0300

    Create javascript1.js

---
+++ web8/javascript1.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.multiple').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      dots: false, 
+      responsive: [
+        {
+            breakpoint: 719, 
+            settings:{
+                slidesToShow: 1, 
+                slidesToScroll: 1
+            }
+        }
+      ]
+    });
+});
