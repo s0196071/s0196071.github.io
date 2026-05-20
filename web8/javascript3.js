35558276b6d28ef5db208ec21e0801e541c6124d
s0196071 <kisyayo@yandex.ru>
2026-05-20 12:59:20 +0300

    Create javascript3.js

---
+++ web8/javascript3.js
@@ -0,0 +1,10 @@
+document.getElementById('menu1').addEventListener('change', function() {
+  if (this.value) {
+    window.location.href = this.value;
+  }
+});
+document.getElementById('menu2').addEventListener('change', function() {
+  if (this.value) {
+    window.location.href = this.value;
+  }
+});
